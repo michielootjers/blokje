675d53980f3c70cedfbdb42bb0be20b77fc5ef48
Michiel Ootjers <michiel@mintlab.nl>
2012-09-12 19:40:39 +0200

    Added standard files

---
+++ Blokje/lib/Blokje/View/TT.pm
@@ -0,0 +1,33 @@
+package Blokje::View::TT;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(TEMPLATE_EXTENSION => '.tt');
+
+=head1 NAME
+
+Blokje::View::TT - TT View for Blokje
+
+=head1 DESCRIPTION
+
+TT View for Blokje.
+
+=head1 SEE ALSO
+
+L<Blokje>
+
+=head1 AUTHOR
+
+Michiel Ootjers,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ Blokje/t/view_JSON.t
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+use Test::More;
+
+BEGIN { use_ok 'Blokje::View::JSON' }
+
+done_testing();
